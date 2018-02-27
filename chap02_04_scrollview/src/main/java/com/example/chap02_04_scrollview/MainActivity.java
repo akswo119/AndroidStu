ce6222c76ab4ee686c23dd8742b3193a4696d66f
BK <akswo119@gmail.com>
2018-02-27 17:09:34 +0900

    구조변경

--- chap02_04_scrollview/src/main/java/com/example/chap02_04_scrollview/MainActivity.java
+++ chap02_04_scrollview/src/main/java/com/example/chap02_04_scrollview/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.chap02_04_scrollview;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    ImageView imgView;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+        imgView = (ImageView) findViewById(R.id.imgView);
+        scrollView.setHorizontalScrollBarEnabled(true);
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable)res.getDrawable(R.drawable.t1);
+        int bitWidth = bitmap.getIntrinsicWidth();
+        int bitHeight = bitmap.getIntrinsicHeight();
+
+        imgView.setImageDrawable(bitmap);
+        imgView.getLayoutParams().width = bitWidth;
+        imgView.getLayoutParams().height = bitHeight;
+    }
+
+    Boolean isFirst = true;
+
+    public void onbtn1Clicked(View v){
+
+        Resources res = getResources();
+
+        if (isFirst) {
+            bitmap = (BitmapDrawable) res.getDrawable(R.drawable.t2);
+        }
+        else
+        {
+            bitmap = (BitmapDrawable) res.getDrawable(R.drawable.t1);
+        }
+
+        isFirst = !isFirst;
+
+        int bitWidth = bitmap.getIntrinsicWidth();
+        int bitHeight = bitmap.getIntrinsicHeight();
+
+        imgView.setImageDrawable(bitmap);
+        imgView.getLayoutParams().width = bitWidth;
+        imgView.getLayoutParams().height = bitHeight;
+
     }
 }
--- chap02_05_framelayout/src/main/java/com/example/chap02_05_framelayout/MainActivity.java
+++ chap02_05_framelayout/src/main/java/com/example/chap02_05_framelayout/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.chap02_05_framelayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView1;
+    ImageView imageView2;
+    int imageIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 =(ImageView)findViewById(R.id.img1);
+        imageView2 =(ImageView)findViewById(R.id.img2);
+    }
+
+    public void onbtn1Clicked(View v){
+
+        if(imageIndex == 0){
+            imageView1.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+            imageIndex = 1;
+        }
+        else if (imageIndex == 1) {
+            imageView1.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+            imageIndex = 0;
+        }
+    }
+
+    public void onbtn1ClickedStart(View v){
+        v.setVisibility(View.INVISIBLE);
     }
 }
